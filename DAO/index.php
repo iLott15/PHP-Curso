f6810880e854368d4c3f7474dad78f9f3a428f43
Ivan Lott <ivanlott@hotmail.com>
2022-03-21 08:21:14 -0300

    update/delete DAO

--- DAO/index.php
+++ DAO/index.php
@@ -21,10 +21,30 @@ require_once("config.php");
 
 //echo $usuario;
 
+/*
+Criando um novo usuário
 $aluno = new Usuario("aluno", "@lun0");
-
 $aluno->insert();
-
 echo $aluno;
+*/
+
+/*
+Alterar um usuário
+$usuario = new Usuario();
+
+$usuario->loadById(8);
+
+$usuario->update("professor", "!@#$%¨&*");
+
+echo $usuario;
+*/
+
+$usuario = new Usuario();
+
+$usuario->loadById(7);
+
+$usuario->delete();
+
+echo $usuario;
 
 ?>
